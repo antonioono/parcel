dd9d3ff19e5a4b56fe04e7d600daf88f0955e98a
Antonio Ono <me@antonioono.com>
2014-11-07 16:52:49 -0500

    new project

---
+++ Parcel/Parcel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Parcel
+//
+//  Created by Antonio Ono on 11/7/14.
+//  Copyright (c) 2014 Antonio Ono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
